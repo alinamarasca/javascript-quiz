faa209f59ed6b7294e2e1719e1014c46961f3d53
HYF <alinamarasca@gmail.com>
2021-07-06 12:09:01 +0200

    plan next btn

--- client/src/components/pages/quiz/next-btn.js
+++ client/src/components/pages/quiz/next-btn.js
@@ -3,6 +3,7 @@ import { nextQuestionButton } from "../../../handlers/next-question-btn.js";
 export const nextButton = () => {
   const button = document.createElement("BUTTON");
   button.innerText = "next";
+  button.classList = "next-btn";
   button.addEventListener("click", nextQuestionButton);
   return button;
 };
--- client/src/handlers/next-question-btn.js
+++ client/src/handlers/next-question-btn.js
@@ -1,3 +1,17 @@
-const nextQuestionButton = () => {
-    console.log('next question please')
+export const nextQuestionButton = (questions) => {
+console.log('next question please');
+
+// const question = state.question[i]
+
+// if (target.nodeName === "BUTTON") {
+// event target button 
+// clear div
+// displayQuestion(question);
+// i++
+//     /*
+//     take same index as on the screen,
+//      changes data, 
+//      click i++ switch to the next
+//     */
+
 }
