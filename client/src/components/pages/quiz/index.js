ad655ea250029ff7c28a280b8009524322a422e1
alinamarasca <75176258+alinamarasca@users.noreply.github.com>
2021-07-09 00:05:51 +0200

    Update index.js

--- client/src/components/pages/quiz/index.js
+++ client/src/components/pages/quiz/index.js
@@ -1,5 +1,8 @@
 import { inputGreeting } from "../../shared/input-greeting.js";
 import { restartButton } from "../../shared/restart-btn.js";
+import { RenderQuiz } from "./render-quiz.js";
+import { NextButton } from "./next-btn.js";
+import { data } from "../../../../data/quiz.js";
 
 /**
  * The quiz page.
@@ -9,7 +12,9 @@ import { restartButton } from "../../shared/restart-btn.js";
 export const quiz = () => {
   const container = document.createElement("div");
   container.className = "body";
-
+  
+  container.appendChild(RenderQuiz(data.currentQuestion));
+  container.appendChild(NextButton());
   container.appendChild(restartButton());
   container.appendChild(inputGreeting());
 
