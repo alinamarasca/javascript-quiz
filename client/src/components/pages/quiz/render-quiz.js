1aa132f645481820d192451573046354e04a4814
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2021-07-11 13:31:37 +0200

    Add handler for answering a question

--- client/src/components/pages/quiz/render-quiz.js
+++ client/src/components/pages/quiz/render-quiz.js
@@ -1,4 +1,5 @@
 import { state } from "../../../init/state.js";
+import { changeAnswer } from "../../../handlers/change-answer";
 
 const RenderQuiz = (questionIndex) => {
   const quizContainer = document.createElement("div");
@@ -6,13 +7,18 @@ const RenderQuiz = (questionIndex) => {
   const currentQuestion = state.questions[questionIndex];
   questions.innerText = currentQuestion.question;
   const answers = document.createElement("ul");
+
+  let answerCounter = 0;
   for (const item of currentQuestion.answers) {
     const li = document.createElement("li");
     li.innerText = item.text;
     const checkbox = document.createElement("input");
+    checkbox.setAttribute("data-answer-index", answerCounter);
     checkbox.setAttribute("type", "checkbox");
+    checkbox.addEventListener("change", changeAnswer);
     li.insertAdjacentElement("afterbegin", checkbox);
     answers.appendChild(li);
+    answerCounter++;
   }
   quizContainer.appendChild(questions);
   quizContainer.appendChild(answers);
